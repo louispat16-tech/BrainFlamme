428f884d7cefcfd2e57e5b52d9635cfd13d5ee3c
louispat16-tech <louispataud16@gmail.com>
2026-08-06 16:18:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ if (!firebase.apps.length) {
 }
 
 // 2. Initialisation des services (Firestore, Database, Auth)
-const database = firebase.database(); // ✅ Utilisez Realtime Database
+const database = firebase.database(); // ✅ Utilisez Realtime Database 
 const auth = firebase.auth();
 
 // --- ÉCOUTEUR DE CONNEXION ---
