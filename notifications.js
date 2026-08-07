f95d5b8e4f881022bfa2bdf747dfa92ac261ae38
louispat16-tech <louispataud16@gmail.com>
2026-08-07 20:20:23 +0200

    Update notifications.js

--- notifications.js
+++ notifications.js
@@ -6,13 +6,13 @@ function demanderPermissionNotification() {
     if ('serviceWorker' in navigator && 'Notification' in window) {
         
         // 1. Enregistrer le fichier arrière-plan sw.js
-        navigator.serviceWorker.register('sw.js')
-            .then((registration) => {
-                console.log('✅ Service Worker enregistré avec succès !', registration);
-            })
-            .catch((err) => {
-                console.error('❌ Erreur d'enregistrement du Service Worker :', err);
-            });
+navigator.serviceWorker.register('sw.js')
+    .then((registration) => {
+        console.log('✅ Service Worker enregistré avec succès !', registration);
+    })
+    .catch((err) => {
+        console.error("❌ Erreur d'enregistrement du Service Worker :", err);
+    });
 
         // 2. Demander la permission
         Notification.requestPermission().then(permission => {
