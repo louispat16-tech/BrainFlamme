f5940c6235f196e9f5f52af4bddeda052f886fd5
louispat16-tech <louispataud16@gmail.com>
2026-08-10 16:55:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1900,3 +1900,5 @@ function saveUserProfileToFirebase(username, streak, xp, level, avatar) {
         lastSeen: Date.now()
     });
 }
+}
+} // 👈 Rajoute celle-ci tout à la fin de tout ton fichier script.js
